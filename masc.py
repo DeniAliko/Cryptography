12a9bd6ef79efc5615c79f880635c7103d3fd154
DeniAliko <denialiko2016@gmail.com>
2024-01-24 09:13:02 -0500

    dicitonarie

--- masc.py
+++ masc.py
@@ -10,11 +10,30 @@ def shift(pt, n):
 
     return encrypted
 
-pt = input("Plain text: ")
-print(shift(pt.replace(" ", "").lower(), int(input("Shift: "))))
-
 def unShift(ct, n):
     return shift(ct.lower(), (-1) * n).lower()
 
+pt = input("Plain text: ")
+print(shift(pt.replace(" ", "").lower(), int(input("Shift: "))))
 ct = input("Cipher text: ")
 print(unShift(ct.replace(" ", "").lower(), int(input("Shift: "))))
+
+def getCaesarDict(n):
+    shiftDict = {}
+    for i in range(len(string.ascii_lowercase)):
+        shiftDict[string.ascii_lowercase[i]] = string.ascii_uppercase[(i+n)%26]
+    return shiftDict
+
+def dictEncrypt(pt, n):
+    ct = ""
+    for char in pt:
+        ct += getCaesarDict(n)[char]
+    return ct
+
+def dictDecrypt(ct, n):
+    return dictEncrypt(ct.lower(), (-1) * n).lower()
+
+pt = input("Plain text: ")
+print(dictEncrypt(pt.replace(" ", "").lower(), int(input("Shift: "))))
+ct = input("Cipher text: ")
+print(dictDecrypt(ct.replace(" ", "").lower(), int(input("Shift: "))))
