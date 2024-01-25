bce7212ae1c147342872546f68627ab5b82aed39
DeniAliko <denialiko2016@gmail.com>
2024-01-25 14:31:31 -0500

    messiest code known to man

--- masc.py
+++ masc.py
@@ -13,10 +13,10 @@ def shift(pt, n):
 def unShift(ct, n):
     return shift(ct.lower(), (-1) * n).lower()
 
-pt = input("Plain text: ")
-print(shift(pt.replace(" ", "").lower(), int(input("Shift: "))))
-ct = input("Cipher text: ")
-print(unShift(ct.replace(" ", "").lower(), int(input("Shift: "))))
+# pt = input("Plain text: ")
+# print(shift(pt.replace(" ", "").lower(), int(input("Shift: "))))
+# ct = input("Cipher text: ")
+# print(unShift(ct.replace(" ", "").lower(), int(input("Shift: "))))
 
 def getCaesarDict(n):
     shiftDict = {}
@@ -34,6 +34,28 @@ def dictDecrypt(ct, n):
     return dictEncrypt(ct.lower(), (-1) * n).lower()
 
 pt = input("Plain text: ")
-print(dictEncrypt(pt.replace(" ", "").lower(), int(input("Shift: "))))
-ct = input("Cipher text: ")
-print(dictDecrypt(ct.replace(" ", "").lower(), int(input("Shift: "))))
+# print(dictEncrypt(pt.replace(" ", "").lower(), int(input("Shift: "))))
+# ct = input("Cipher text: ")
+# print(dictDecrypt(ct.replace(" ", "").lower(), int(input("Shift: "))))
+
+def masc(text, subD):
+    output = ""
+    for char in text:
+        if char in subD.keys():
+            output += subD.get(char, '\U0001F4A9')
+    return output
+
+# print(masc("ABCD", {"A": "B", "B": "C", "C": "D"}))
+
+def bruteForce(CT):
+    for i in range(1, 26):
+        shiftDict = {}
+        for j in range(len(string.ascii_lowercase)):
+            shiftDict[string.ascii_lowercase[j]] = string.ascii_lowercase[(j+i)%26]
+
+        decode = ""
+        for char in CT:
+            decode += shiftDict[char]
+        print(decode)
+
+bruteForce(dictEncrypt(pt.replace(" ", "").lower(), int(input("Shift: "))).lower())
