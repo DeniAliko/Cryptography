e8f36578f59f3e1fcbe374ee8fd563318f08b221
DeniAliko <denialiko2016@gmail.com>
2024-01-30 20:15:22 -0500

    hw

--- fileWork.py
+++ fileWork.py
@@ -5,3 +5,10 @@ name = "caesarTest.txt"
 
 ct = dl.openFile(path, name)[0]
 dl.printList([i + "\n" for i in dl.bruteForce(ct)])
+
+name = "codewordTest.txt"
+ct = dl.openFile(path, name)[0]
+codePhrase = "marcuscicero"
+encryptDict = dl.generateCodewordDict(codePhrase)
+decryptDict = dl.reverseDict(encryptDict)
+print(dl.masc(ct, decryptDict))
